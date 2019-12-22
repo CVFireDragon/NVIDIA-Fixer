4507a60989723b91b0602ee0f743741250416be9
Luke <39099814+CVFireDragon@users.noreply.github.com>
2019-12-21 19:21:07 -0500

    Update NVIDIA Fixer.bat

--- NVIDIA Fixer.bat
+++ NVIDIA Fixer.bat
@@ -6,8 +6,13 @@ TIMEOUT /T 2 >NUL
 echo Starting NVIDIA Container LocalSystem...
 sc start NvContainerLocalSystem >NUL
 ECHO.
-echo -----------------------------------------------------------
+echo ----------------------------------------------------------------
 ECHO.
 echo Thanks for using the Automated NVIDIA Fixing Tool!
+echo Please wait for a minute and then try to use GeForce Experience.
+ECHO. 
+echo ----------------------------------------------------------------
 echo Tool created by @CVFD_ / CVFD#6480 / https://bit.ly/2EIDMxc
+ECHO.
+echo ----------------------------------------------------------------
 pause
